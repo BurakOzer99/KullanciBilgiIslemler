8166f0a240d817e230ec2d1e2fc9e0a539ee67d6
Burak <118886894+BurakOzer99@users.noreply.github.com>
2023-01-24 13:28:31 +0300

    KullanciBilgiIslemler

---
+++ KullaniciIslemleri-1/KullaniciIslemleri-1/KullaniciIslemleri/KullaniciIslemleri/frmAnaEkran.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace KullaniciIslemleri
+{
+    partial class frmAnaEkran
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnKullaniciAra = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ogrnoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.soyadDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ogrenciBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataDataSet = new KullaniciIslemleri.dataDataSet();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.ogrenciTableAdapter = new KullaniciIslemleri.dataDataSetTableAdapters.OgrenciTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ogrenciBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(268, 249);
+            this.btnEkle.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(98, 31);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = " Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(268, 284);
+            this.btnSil.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(98, 31);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = " Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(268, 319);
+            this.btnGuncelle.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(98, 31);
+            this.btnGuncelle.TabIndex = 4;
+            this.btnGuncelle.Text = " Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnKullaniciAra
+            // 
+            this.btnKullaniciAra.Location = new System.Drawing.Point(279, 19);
+            this.btnKullaniciAra.Margin = new System.Windows.Forms.Padding(2);
+            this.btnKullaniciAra.Name = "btnKullaniciAra";
+            this.btnKullaniciAra.Size = new System.Drawing.Size(124, 25);
+            this.btnKullaniciAra.TabIndex = 5;
+            this.btnKullaniciAra.Text = "Okul No Göre Ara";
+            this.btnKullaniciAra.UseVisualStyleBackColor = true;
+            this.btnKullaniciAra.Click += new System.EventHandler(this.btnKullaniciAra_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 259);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Öğrenci No";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SunkenHorizontal;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ogrnoDataGridViewTextBoxColumn,
+            this.adDataGridViewTextBoxColumn,
+            this.soyadDataGridViewTextBoxColumn,
+            this.telDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.ogrenciBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 62);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(443, 167);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ogrnoDataGridViewTextBoxColumn
+            // 
+            this.ogrnoDataGridViewTextBoxColumn.DataPropertyName = "ogr_no";
+            this.ogrnoDataGridViewTextBoxColumn.HeaderText = "ogr_no";
+            this.ogrnoDataGridViewTextBoxColumn.Name = "ogrnoDataGridViewTextBoxColumn";
+            // 
+            // adDataGridViewTextBoxColumn
+            // 
+            this.adDataGridViewTextBoxColumn.DataPropertyName = "ad";
+            this.adDataGridViewTextBoxColumn.HeaderText = "ad";
+            this.adDataGridViewTextBoxColumn.Name = "adDataGridViewTextBoxColumn";
+            // 
+            // soyadDataGridViewTextBoxColumn
+            // 
+            this.soyadDataGridViewTextBoxColumn.DataPropertyName = "soyad";
+            this.soyadDataGridViewTextBoxColumn.HeaderText = "soyad";
+            this.soyadDataGridViewTextBoxColumn.Name = "soyadDataGridViewTextBoxColumn";
+            // 
+            // telDataGridViewTextBoxColumn
+            // 
+            this.telDataGridViewTextBoxColumn.DataPropertyName = "tel";
+            this.telDataGridViewTextBoxColumn.HeaderText = "tel";
+            this.telDataGridViewTextBoxColumn.Name = "telDataGridViewTextBoxColumn";
+            // 
+            // ogrenciBindingSource
+            // 
+            this.ogrenciBindingSource.DataMember = "Ogrenci";
+            this.ogrenciBindingSource.DataSource = this.dataDataSet;
+            // 
+            // dataDataSet
+            // 
+            this.dataDataSet.DataSetName = "dataDataSet";
+            this.dataDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 284);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Ad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(89, 312);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Soyad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 338);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Telefon";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(132, 256);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(132, 282);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(132, 309);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(132, 335);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 15;
+            // 
+            // ogrenciTableAdapter
+            // 
+            this.ogrenciTableAdapter.ClearBeforeFill = true;
+            // 
+            // frmAnaEkran
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 382);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnKullaniciAra);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frmAnaEkran";
+            this.Text = "frmAnaEkran";
+            this.Load += new System.EventHandler(this.frmAnaEkran_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ogrenciBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnKullaniciAra;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private dataDataSet dataDataSet;
+        private System.Windows.Forms.BindingSource ogrenciBindingSource;
+        private dataDataSetTableAdapters.OgrenciTableAdapter ogrenciTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ogrnoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn soyadDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telDataGridViewTextBoxColumn;
+
+    }
+}
---
+++ KullaniciIslemleri-1/KullaniciIslemleri-1/KullaniciIslemleri/KullaniciIslemleri/frmAnaEkran.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace KullaniciIslemleri
+{
+    public partial class frmAnaEkran : Form
+    {
+        public frmAnaEkran()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string strSaglayici = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\data\data.accdb";
+            OleDbConnection baglanti = new OleDbConnection(strSaglayici);
+            baglanti.Open();
+
+            string sorgu = "insert into Ogrenci(ogr_no,ad,soyad,tel) values(@ogr_no,@ad,@soyad,@tel)";
+            OleDbCommand cmd = new OleDbCommand(sorgu, baglanti);
+
+            cmd.Parameters.AddWithValue("ogr_no", textBox2.Text);
+            cmd.Parameters.AddWithValue("ad", textBox3.Text);
+            cmd.Parameters.AddWithValue("soyad", textBox4.Text);
+            cmd.Parameters.AddWithValue("tel", textBox5.Text);
+            cmd.ExecuteNonQuery();
+            Listelee();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            dataGridView1.Refresh();
+            baglanti.Close();
+
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            string strSaglayici = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\data\data.accdb";
+            OleDbConnection baglanti = new OleDbConnection(strSaglayici);
+            baglanti.Open();
+            string sorgu = "Delete from Ogrenci where ogr_no=@ogr_no";
+
+            OleDbCommand cmd = new OleDbCommand(sorgu, baglanti);
+            cmd.Parameters.AddWithValue("ogr_no", textBox2.Text);
+            cmd.ExecuteNonQuery();
+            dataGridView1.Refresh();
+
+        }
+
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            string strSaglayici = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\data\data.accdb";
+            OleDbConnection baglanti = new OleDbConnection(strSaglayici);
+            baglanti.Open();
+
+            string sorgu = "update Ogrenci set ad=@ad, soyad=@soyad where ogr_no=@ogr_no";
+            OleDbCommand cmd = new OleDbCommand(sorgu, baglanti);
+
+            cmd.Parameters.AddWithValue("ogr_no", textBox2.Text);
+            cmd.Parameters.AddWithValue("ad", textBox3.Text);
+            cmd.Parameters.AddWithValue("soyad", textBox4.Text);
+            cmd.Parameters.AddWithValue("tel", textBox5.Text);
+            cmd.ExecuteNonQuery();
+            Listelee();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            dataGridView1.Refresh();
+            baglanti.Close();
+
+        }
+
+        private void btnKullaniciAra_Click(object sender, EventArgs e)
+        {
+            string strSaglayici = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\data\data.accdb";
+            OleDbConnection baglanti = new OleDbConnection(strSaglayici);
+            baglanti.Open();
+            DataTable tbl = new DataTable();
+            string vara, cumle;
+            vara =textBox1.Text;
+            cumle = "Select * from Ogrenci where ogr_no like '%" + textBox1.Text + "%'";
+            OleDbDataAdapter adptr = new OleDbDataAdapter(cumle, baglanti);
+            adptr.Fill(tbl);
+            baglanti.Close();
+            dataGridView1.DataSource = tbl;
+
+            textBox1.Clear();
+            
+
+  
+        }
+
+        private void frmAnaEkran_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dataDataSet.Ogrenci' table. You can move, or remove it, as needed.
+            this.ogrenciTableAdapter.Fill(this.dataDataSet.Ogrenci);
+            string strSaglayici = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\data\data.accdb";
+            OleDbConnection baglanti = new OleDbConnection(strSaglayici);
+            baglanti.Open();
+            DataTable tablo = new DataTable();
+            dataGridView1.DataSource = tablo;
+            DataSet dtst = new DataSet();           
+            OleDbDataAdapter adtr = new OleDbDataAdapter("select * From Ogrenci", baglanti);
+            adtr.Fill(dtst, "Ogrenci");
+            dataGridView1.DataSource = dtst.Tables["Ogrenci"];
+            adtr.Dispose();
+            dataGridView1.Refresh();
+            baglanti.Close();
+        }
+
+        private void Listelee()
+        {
+            string strSaglayici = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\data\data.accdb";
+            OleDbConnection baglanti = new OleDbConnection(strSaglayici);
+            baglanti.Open();
+            string sql = "select * from Ogrenci";
+            OleDbCommand komut = new OleDbCommand(sql, baglanti);
+            OleDbCommandBuilder commandBuilder = new OleDbCommandBuilder();
+            DataTable tablom = new DataTable();
+            OleDbDataAdapter da = new OleDbDataAdapter(komut);
+            da.Fill(tablom);
+            dataGridView1.DataSource = tablom;
+            
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            textBox5.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+        }
+
+
+
+
+
+      
+    }
+}
